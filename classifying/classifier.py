3ce8b7f47aa7150b3f0974a4dfe8f75b1a7248a3
Avram <ad@dermer.co.il>
2025-09-09 17:56:28 +0300

    start level d

---
+++ classifying/classifier.py
@@ -0,0 +1,9 @@
+
+
+
+
+
+class Classifier:
+
+    def __init__(self):
+        pass
---
+++ classifying/menager.py
@@ -0,0 +1,46 @@
+from tools import KafkaConsumerRepo ,ESRepository ,MongoRepository  , Logger
+import threading
+import time
+from .classifier import Classifier 
+
+
+es:ESRepository|None = None
+mongodb:MongoRepository|None = None
+classifier:Classifier|None = None
+
+def messageHandler(topic:str,message:dict):
+    # print(f"topic: {topic}, masseg: {message}")
+    u_id = message["u_id"]
+    # classifier.to_classing(u_id)
+    
+
+class Manager:
+
+    def __init__(self):
+        self.kafka = KafkaConsumerRepo('to_transcribing')
+        global es , mongodb ,classifier
+        classifier = Classifier()
+        es = ESRepository()
+        mongodb = MongoRepository()
+
+    def start_lisane(self,messageHandler):
+        t = threading.Thread(target=self.kafka.listen,args=(1,messageHandler),daemon=True)
+        t.start()
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            print("stop thred")
+
+    
+
+
+
+
+    
+if __name__ == "__main__":
+    m = Manager()
+
+    m.start_lisane(messageHandler)
+
+    # mongodb.get_all()
