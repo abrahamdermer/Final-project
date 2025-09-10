88d2cc65a4cf464dfaf0aedf0b1cd93e7e856a33
Avram <ad@dermer.co.il>
2025-09-10 11:33:13 +0300

    for prepross

---
+++ Preprocessing/config.py
@@ -0,0 +1,5 @@
+import os
+
+# # Basic connection
+FILE_PATH: str = os.getenv("FILE_PATH", "C:\podcasts")
+TARGET_TOPIC_NAME: str = os.getenv("TARGET_TOPIC_NAME", "to_saving")
--- Preprocessing/manager.py
+++ Preprocessing/manager.py
@@ -2,14 +2,15 @@ from os import listdir
 from os.path import isfile, join
 from .MD_builder import MD_Builder
 from tools import KafkaProducerRepo
+from . import config
 # from tools import  Logger
 
 
 class Manager:
 
-    def __init__(self,mypath = "C:\podcasts"):
+    def __init__(self,mypath:str = config.FILE_PATH):
         # self.logger = Logger.get_logger()
-        self.topic_name = "to_saving"
+        self.topic_name = config.TARGET_TOPIC_NAME
         self.mypath =mypath
         self.files = [f for f in listdir(self.mypath) if isfile(join(self.mypath, f))]
         self.kafka = KafkaProducerRepo()
